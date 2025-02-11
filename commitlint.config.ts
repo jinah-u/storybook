20583e5973e7d9b00e43f6a857708b6f9bb9ba3c
jinah <jinah@datau.co.kr>
2025-02-11 16:48:23 +0900

    refactor(husky): type 단독 사용 허용 규칙 삭제

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,13 +2,6 @@ import { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  //단일 커밋 메시지도 검사
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w+)(?:\((.*)\))?:?\s*(.*)$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
   rules: {
     'type-enum': [
       2,
@@ -30,8 +23,6 @@ const Configuration: UserConfig = {
       ],
     ],
     'type-case': [2, 'always', 'kebab-case'],
-    'type-empty': [0],
-    'subject-empty': [0],
     'body-max-length': [0],
     'footer-max-length': [0],
   },
